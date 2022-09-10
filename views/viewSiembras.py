7e84d8d8fd0272d775a89a86927e55a67270dba6
Daza <williamdaza04@gmail.com>
2022-09-10 11:16:56 -0500

    Ajuste vista

--- views/viewSiembras.py
+++ views/viewSiembras.py
@@ -5,13 +5,14 @@ Created on Sat Sep 10 08:45:15 2022
 @author: WINDOWS 11
 """
 
+from tkinter import ttk
 import tkinter as tk
 import tkinter.font as tkFont
 
 class App:
     def __init__(self, root):
         #setting title
-        root.title("Siembras Antioquia")
+        root.title("undefined")
         #setting window size
         width=600
         height=500
@@ -29,6 +30,20 @@ class App:
         GLabel_749["justify"] = "center"
         GLabel_749["text"] = "Siembras Vigiladas por Corantioquia"
         GLabel_749.place(x=120,y=30,width=358,height=38)
+
+        GLabel_777=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_777["font"] = ft
+        GLabel_777["fg"] = "#333333"
+        GLabel_777["justify"] = "center"
+        GLabel_777["text"] = "Vereda-Municipio"
+        GLabel_777.place(x=10,y=90,width=107,height=30)
+
+        combo = ttk.Combobox()      
+        ft = tkFont.Font(family='Times',size=10)
+        combo["font"] = ft
+        combo["justify"] = "center"
+        combo.place(x=130,y=90,width=171,height=30)
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
