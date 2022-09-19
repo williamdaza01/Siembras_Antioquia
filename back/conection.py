19e5714baa7aa81a4178d56e6eb60925e608dadd
Daza <williamdaza04@gmail.com>
2022-09-19 15:35:38 -0500

    Se hacen cambion en la conexion

--- back/conection.py
+++ back/conection.py
@@ -1,9 +1,12 @@
 import sqlite3
+import Siembra as Siembra
 
 conexion=sqlite3.connect("./database/siembrasDB.sqlite")
-try:
-    conexion.execute("""""")
-    print("se creo la tabla articulos")                        
-except sqlite3.OperationalError:
-    print("error")                     
-conexion.close()
+salida = Siembra
+consultasql = "select s.codigo, s.fecha, s.total_hectareas, s.total_arbole, s.codigo_vereda, s.codigo_arbol, s.codigo_contratista from siembras"
+
+def __getSiembra__(codigo) -> Siembra:
+    try:
+        conexion.execute(consultasql)
+    except:
+        print("error")
